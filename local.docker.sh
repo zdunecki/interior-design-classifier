b1c54ead7a43166729405961fc83a56c5aa0ddc2
zdunecki <zdunekhere@gmail.com>
2019-11-30 11:27:44 +0100

    fix(train): batch size

--- local.docker.sh
+++ local.docker.sh
@@ -1,21 +1,21 @@
 function build() {
-  docker build -f train.local.dockerfile \
-  -t registry.gitlab.com/topifyresearch/interior-design-mnist \
+  sudo docker build -f train.local.dockerfile \
+  -t registry.gitlab.com/topifyresearch/mnist \
   --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" \
   --build-arg SSH_PUB_KEY="$(cat ~/.ssh/id_rsa.pub)" \
   .
 }
 
 function train() {
-  docker run \
+  sudo docker run \
   -e IMAGE_WIDTH=150 \
   -e IMAGE_HEIGHT=150 \
   -e MODEL_OUTPUT="./output/model.json" \
   -e WEIGHTS_OUTPUT="./output/weights.h5" \
-  -e BATCH_SIZE=1 \
+  -e BATCH_SIZE=128 \
   -v "$PWD"/data/test:/app/data \
   -v "$PWD"/output:/app/output \
-  registry.gitlab.com/topifyresearch/interior-design-mnist
+  registry.gitlab.com/topifyresearch/mnist
 }
 
 $1
